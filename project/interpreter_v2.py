ed4b4392278fe03d30051e844d941509810d9346
Mykola-MacBook <niko050139892@gmail.com>
2020-02-23 00:59:59 +0200

    It is ALIVE!!!

--- project/interpreter_v2.py
+++ project/interpreter_v2.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.vector import Vector
 
 from kivy.config import Config
 
@@ -8,11 +9,37 @@ Config.set("graphics", "resizable", "0")
 Config.set("graphics", "width", "1000")
 Config.set("graphics", "height", "650")
 
+
+console_log = "CONSOLE LOG:\n"
+
+"""def move_up(pos):
+    pos[1] += 100
+    return pos"""
+
+def move_up(self):
+    self.robot.pos[0] += 100
+
+
 class MyProgram(Widget):
     code = ObjectProperty(None)
+    console = ObjectProperty(None)
+    robot = ObjectProperty(None)
 
     def get_code(self):
         print(self.code.text)
+
+    """def move_robot(self):
+        pos = self.robot.pos
+        pos = move_up(pos)
+
+        self.robot.pos = pos"""
+
+    def move_robot(self):
+        move_up(self)
+        #self.robot.pos[0] += 100
+
+    def set_console(self):
+        self.console.text = console_log
 
 
 class LightBot_v2App(App):
